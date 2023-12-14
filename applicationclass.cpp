325a279b0d052a85de10e5c419facb43f2e1b1a7
RibeiroLucas <lucasribeiro1024b@gmail.com>
2023-12-13 21:57:18 -0300

    dxd11 up and running

--- applicationclass.cpp
+++ applicationclass.cpp
@@ -6,6 +6,7 @@
 
 ApplicationClass::ApplicationClass()
 {
+	m_Direct3D = 0;
 }
 
 
@@ -21,6 +22,18 @@ ApplicationClass::~ApplicationClass()
 
 bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 {
+	bool result;
+
+
+	// Create and initialize the Direct3D object.
+	m_Direct3D = new D3DClass;
+
+	result = m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize Direct3D", L"Error", MB_OK);
+		return false;
+	}
 
 	return true;
 }
@@ -28,6 +41,13 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
 void ApplicationClass::Shutdown()
 {
+	// Release the Direct3D object.
+	if (m_Direct3D)
+	{
+		m_Direct3D->Shutdown();
+		delete m_Direct3D;
+		m_Direct3D = 0;
+	}
 
 	return;
 }
@@ -35,6 +55,14 @@ void ApplicationClass::Shutdown()
 
 bool ApplicationClass::Frame()
 {
+	bool result;
+
+	// Render the graphics scene.
+	result = Render();
+	if (!result)
+	{
+		return false;
+	}
 
 	return true;
 }
@@ -42,6 +70,11 @@ bool ApplicationClass::Frame()
 
 bool ApplicationClass::Render()
 {
+	// Clear the buffers to begin the scene.
+	m_Direct3D->BeginScene(1.0f, 1.0f, 0.0f, 1.0f);
+
+	// Present the rendered scene to the screen.
+	m_Direct3D->EndScene();
 
 	return true;
 }
